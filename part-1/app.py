ab6771421acbb26d8cb900818c58b5b7bbf71fc6
affanm16 <affan24748@gmail.com>
2024-06-08 23:59:06 +0530

    adding different ways of end poitns

--- part-1/app.py
+++ part-1/app.py
@@ -6,6 +6,7 @@ my_app= Flask(__name__)
 
 #creating an endpoint
 @my_app.route("/")
+@my_app.route("/home") #for same function we can have multiple nodes
 def home_page():
     return "<h1> Welcome to the Home Page!</h1>" #h1-html's header tag
 
@@ -14,6 +15,15 @@ def home_page():
 def about_page():
     return "<h1> Welcome to the About Page!</h1>" #h1-html's header tag
 
+@my_app.route("/welcome/<name>")
+def welcome_page(name):
+    return f"<h1>Hi {name.title()},You are welcome to this page</h1>"
+#title function capitalizes the name(i.e string)
+
+
+@my_app.route("/addition/<int:num1>/<int:num2>")
+def addition_two_nums(num1,num2):
+    return f"<h1>Addition of {num1} and {num2} is {num2+num1}</h1>"
 
 #one way to run the flask app
 if __name__=="__main__":
