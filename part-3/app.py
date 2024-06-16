090ec0230b961be0e22ca6cce7b2d7cbd3e4a70d
affanm16 <affan24748@gmail.com>
2024-06-16 23:49:24 +0530

    adding code for jinja,templates and inheritance

--- part-3/app.py
+++ part-3/app.py
@@ -1,5 +1,7 @@
 #importing the Flask Class from flask library
-from flask import Flask ,render_template
+from flask import Flask ,render_template,url_for
+
+from employees import employees_data
 
 #making an object of the flask class
 my_app= Flask(__name__)
@@ -8,12 +10,29 @@ my_app= Flask(__name__)
 @my_app.route("/")
 @my_app.route("/home") #for same function we can have multiple nodes
 def home_page():
-    return render_template("home.html")
+    return render_template(
+        "home.html",
+        title="Home")  #title is parameter
 
 #creating another endpoint
 @my_app.route("/about")
 def about_page():
-    return render_template("about.html")
+    return render_template("about.html",title="About")
+
+@my_app.route("/evaluate/<int:num>")
+def evaluate(num):
+    return render_template(
+        "evaluate.html",
+        title="Evaluate",
+        number=num
+    )
+
+@my_app.route("/employees")
+def employees():
+    return render_template(
+        "employees.html",
+        emps="employees_data"
+    )
 
 #one way to run the flask app
 if __name__=="__main__":
---
+++ part-3/employees.py
@@ -0,0 +1,27 @@
+employees_data={
+    1:{
+        "name":"Affan",
+        "age":20,
+        "position":"CEO"
+    },
+    2:{
+        "name":"Imtiyaz",
+        "age":45,
+        "position":"labour"
+    },
+    3:{
+        "name":"Zaid",
+        "age":10,
+        "position":"waiter"
+    },
+    4:{
+        "name":"Mubashir",
+        "age":90,
+        "position":"UI/UX designer"
+    },
+    5:{
+        "name":"Arun",
+        "age":20,
+        "position":"Project Manager"
+    }
+}
