8c11cc183be03d75a19ebf67f585dfbba6f91085
affanm16 <affan24748@gmail.com>
2024-06-29 21:06:31 +0530

    adding code for login and signup pages

--- part-4/app.py
+++ part-4/app.py
@@ -1,7 +1,9 @@
 from flask import (
     Flask,
     render_template,
-    url_for
+    url_for,
+    redirect,
+    flash
     )
 from forms import SignupForm,LoginForm
 app =Flask(__name__)
@@ -15,15 +17,28 @@ def home():
     return render_template("home.html",title="HOME")
 
 #end point for signup
-@app.route("/signup")
+@app.route("/signup" , methods=["GET", "POST"])
 def signup():
     form=SignupForm()
+    if form.validate_on_submit():
+        flash(f"{form.username.data} Successfully Registered")
+        return redirect(url_for("home"))
+    
     return render_template("signup.html",title="SIGN UP",form=form)
 
 #end point for login
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form=LoginForm()
+    email=form.email.data
+    pw=form.password.data
+    if form.validate_on_submit():
+        if email=="abc@gmail.com" and pw=="123":
+            flash("Logged in Successfully")
+            return redirect(url_for("home"))
+        else:
+          flash("Incorrect Email or Password")  
+
     return render_template("login.html",title="LOGIN",form=form
     )
 
